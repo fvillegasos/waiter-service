4c3bace553d453f1855e2b8e0b174aad26ed6ad4
fvillegasos <154078493+fvillegasos@users.noreply.github.com>
2024-11-05 18:22:18 +0100

    main: first code generation from Spring Initz

---
+++ src/test/java/com/fvillegas/kafka/waiter_service/WaiterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fvillegas.kafka.waiter_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
